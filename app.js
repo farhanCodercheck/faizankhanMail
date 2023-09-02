40f285fe20ac66780c44a8fed2a4fd272770cb4c
farhanCodercheck <143864141+farhanCodercheck@users.noreply.github.com>
2023-09-02 11:06:42 +0530

    first uplode

---
+++ app.js
@@ -0,0 +1,16 @@
+// // Import required modules
+// const express = require('express');
+
+// // Create an Express application
+// const app = express();
+
+// // Define a route handler for the root URL
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
+// // Start the server
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
---
+++ server.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'k81183689@gmail.com',
+      pass: 'gokouybrxscmlvto',
+    },
+  });
+
+  const mailDetails = {
+    from: 'k81183689@gmail.com',
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailDetails, (err, data) => {
+    if (err) {
+      console.error('Error sending email:', err);
+      res.status(500).json({ error: 'Error sending email' });
+    } else {
+      console.log('Email sent successfully');
+      res.json({ success: 'Email sent successfully' });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+
